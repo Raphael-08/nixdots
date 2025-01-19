7a7ab2719001a4bd71ea896860e11c6ec016faef
Raphael-08 <mohanvarma130@gmail.com>
2025-01-19 18:39:32 +0530

    merged to boot.nix and relocated to modules/core

---
+++ modules/core/boot.nix
@@ -0,0 +1,32 @@
+{ pkgs
+, config
+, ...
+}:
+{
+  boot = {
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    kernelParams = [
+      "nowatchdog"
+      "modprobe.blacklist=iTCO_wdt" # watchdog for Intel
+    ];
+
+    # This is for OBS Virtual Cam Support
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        enable = true;
+        devices = [ "nodev" ];
+        efiSupport = true;
+        useOSProber = true;
+        theme = inputs.catppuccin-grub + "/src/catppuccin-mocha-grub-theme";
+      };
+    };
+    supportedFilesystems.ntfs = true;
+  };
+}
