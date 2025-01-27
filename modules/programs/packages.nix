5f089cc94001a31aa740c78fa4f6d57e2f0e1537
Raphael-08 <106021733+Raphael-08@users.noreply.github.com>
2025-01-27 10:13:37 +0530

    added nix language support for vscode

--- modules/programs/packages.nix
+++ modules/programs/packages.nix
@@ -2,36 +2,46 @@
 {
   # Install Packages For The User
   home.packages = with pkgs; [
-    libvirt 
-    swww 
-    grim 
+    libvirt
+    swww
+    grim
     file-roller
-    swaynotificationcenter 
-    rofi-wayland 
-    imv 
+    swaynotificationcenter
+    rofi-wayland
+    imv
     transmission_4-gtk
     mpv
-    gimp 
-    obs-studio 
-    rustup 
-    audacity 
+    gimp
+    obs-studio
+    rustup
+    audacity
     pavucontrol
-    tree 
+    tree
     protonup-qt
-    font-awesome 
-    swayidle 
+    font-awesome
+    swayidle
     vlc
     vscode
     zoxide
     glxinfo
-    (ollama.override { acceleration = "cuda";})
+    (ollama.override { acceleration = "cuda"; })
     inputs.zen-browser.packages.${pkgs.system}.default
-
 
     # web dev
     corepack_23
     nodejs_23
     go
+
+    # make
+    gnumake42
+
+    #language support for nix
+    ## nix language server
+    nil
+
+    ## nix formatter
+    nixfmt-rfc-style
+
   ];
 
   programs.gh.enable = true;
